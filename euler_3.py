299cd795fbe48861cad7a795ba9dc4b2cd0cec43
Greg Thorson <Engywook2005@gmail.com>
2024-05-07 22:40:57 -0400

    now working. Slow, but working

--- euler_3.py
+++ euler_3.py
@@ -4,7 +4,7 @@ def is_prime(n):
     upper_limit = math.sqrt(n)
     pointer = 2
     while pointer <= upper_limit:
-        print('pointer is {}'.format(pointer))
+        # print('pointer is {}'.format(pointer))
         if(n % pointer == 0):
             return False
         pointer += 1
@@ -17,18 +17,19 @@ def find_prime_factor(n):
 
     while divisor <= upper_limit:
         if is_prime(divisor):
-            print('dividing by {}'.format(divisor))
+        #    print('dividing by {}'.format(divisor))
             if n % divisor == 0:
                 prime_factors.append(divisor)
+                quotient = n / divisor
+                if is_prime(quotient):
+                    prime_factors.append(math.floor(quotient))
+                print('{} by {} found'.format(divisor, math.floor(quotient)))    
         divisor += 1
 
     return prime_factors    
 
-prime_factors = find_prime_factor(60)
+print(find_prime_factor(600851475143))
 
-print(prime_factors)
+#print(find_prime_factor(34))
 
-
-# print(find_prime_factor(600851475143))
-
-print(is_prime(25))
+#print(is_prime(25))
