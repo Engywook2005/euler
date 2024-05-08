4b7e7d3c4eb50e7f98475bfb5700fb9f27cc0e94
Greg Thorson <Engywook2005@gmail.com>
2024-05-07 22:15:40 -0400

    Without the seive

--- euler_3.py
+++ euler_3.py
@@ -1,35 +1,34 @@
 import math
 
-def find_next_pointer(n, list):
-    idx = list.index(n)
-    if n < len(list):
-        return list[idx + 1]
-    return - 1
-
-
-def erastothenes(n):
-    upper_limit = math.floor(math.sqrt(n))
-    all_primes = list(range(2, n))
+def is_prime(n):
+    upper_limit = math.sqrt(n)
     pointer = 2
-    while pointer < upper_limit and pointer > 0:
+    while pointer <= upper_limit:
         print('pointer is {}'.format(pointer))
-        for num in all_primes:
-            if num != pointer and num % pointer == 0: 
-                print('removing {}'.format(num))
-                all_primes.remove(num)
-        pointer = find_next_pointer(pointer, all_primes)        
-    return all_primes
+        if(n % pointer == 0):
+            return False
+        pointer += 1
+    return True        
 
 def find_prime_factor(n):
-    all_primes = erastothenes(n)
-    print(all_primes)
+    upper_limit = math.sqrt(n)
+    prime_factors = []
+    divisor = 2
 
-    while len(all_primes) > 0:
-        candidate = all_primes.pop()
-        if n % candidate == 0:
-            return candidate
-        
-    return n
+    while divisor <= upper_limit:
+        if is_prime(divisor):
+            print('dividing by {}'.format(divisor))
+            if n % divisor == 0:
+                prime_factors.append(divisor)
+        divisor += 1
 
-print(find_prime_factor(101))      
+    return prime_factors    
+
+prime_factors = find_prime_factor(60)
+
+print(prime_factors)
+
+
 # print(find_prime_factor(600851475143))
+
+print(is_prime(25))
